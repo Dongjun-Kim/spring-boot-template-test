6ae7495c8fc0aaff2da2e7b5707324e9c12f3046
Dongjun-Kim <kdj8036@naver.com>
2015-06-12 19:38:06 +0900

    bower setting 및 thymeleaf 초기 설정

--- src/main/java/kr/co/elevenst/controller/MainController.java
+++ src/main/java/kr/co/elevenst/controller/MainController.java
@@ -1,15 +1,20 @@
 package kr.co.elevenst.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class MainController {
-	@RequestMapping("/greeting")
-	public String greeting(
+	@RequestMapping("/")
+	public ModelAndView index(
 			@RequestParam(value = "name", defaultValue = "World") String name) {
+		
 		System.out.println("adsfasdfa");
-		return "testasdfasdfsadfasdf";
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("test", "djkim");
+		mav.addObject("test2", "11번가");
+		return mav;
 	}
 }
